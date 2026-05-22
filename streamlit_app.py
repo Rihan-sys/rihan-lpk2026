22d7ce35c965e84c86a8aa5c0f24270aa21b689b
Rihan-sys <iisiswatinar@gmail.com>
2026-05-22 10:39:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,15 @@ st.title("🎈 Aplikasi Rihan")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+import pandas as pd
+
+st.write(1234)
+st.write(
+    pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+)
