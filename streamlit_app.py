dbc3596fed827eca0c5dfbffb6f2eaa8dafbea47
Rihan-sys <iisiswatinar@gmail.com>
2026-05-21 23:07:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,16 @@ st.title("🎈 Aplikasi Rihan")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import altair as alt
+import pandas as pd
 import streamlit as st
+from numpy.random import default_rng as rng
 
-st.write("1 + 1 = ", 2)
-st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
+df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.write(chart)
